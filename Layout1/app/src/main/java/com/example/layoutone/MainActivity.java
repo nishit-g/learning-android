22bec942b8c27871579145374c768b2418de276d
shinXan <nishit.gupta6708@gmail.com>
2019-10-09 15:09:05 +0530

    update main_xml

--- Layout1/app/src/main/java/com/example/layoutone/MainActivity.java
+++ Layout1/app/src/main/java/com/example/layoutone/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] product_texts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
